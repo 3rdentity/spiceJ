011162fc640f392c19836c5a58ba92c386d26ffe
Michael Borkowski <michael@borkowski.at>
2015-02-19 18:58:40 +0100

    (core) DelayedInputStream

---
+++ spiceJ/core/src/main/java/at/borkowski/spicej/streams/DelayedInputStream.java
@@ -0,0 +1,114 @@
+package at.borkowski.spicej.streams;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+import at.borkowski.spicej.WouldBlockException;
+import at.borkowski.spicej.impl.SimulationTickSource;
+import at.borkowski.spicej.ticks.TickListener;
+
+public class DelayedInputStream extends InputStream implements TickListener {
+
+   private final InputStream in;
+   private final int delay;
+   private final byte[] buffer;
+
+   private long currentTick;
+
+   private int currentVirtualEnd = 0;
+   private int start = 0, end = 0;
+   private Queue<Long> tickMarks = new LinkedList<>();
+   private Map<Long, Integer> tick_virtualEnd = new HashMap<>();
+
+   public DelayedInputStream(SimulationTickSource t, InputStream in, int delay, int bufferSize) {
+      t.addListener(this);
+
+      this.in = in;
+      this.delay = delay;
+      this.buffer = new byte[bufferSize];
+   }
+
+   @Override
+   public int read() throws IOException {
+      if (delay == 0)
+         handleNewData();
+
+      if (bufferedBytes(currentVirtualEnd) == 0)
+         throw new WouldBlockException();
+
+      byte b = buffer[start++];
+      if (start >= buffer.length)
+         start -= buffer.length;
+
+      return b & 0xFF;
+   }
+
+   int bufferedBytes() {
+      return bufferedBytes(end);
+   }
+
+   int bufferedBytes(int effectiveEnd) {
+      if (start == effectiveEnd)
+         return 0;
+      else if (start < effectiveEnd)
+         return effectiveEnd - start;
+      else
+         return effectiveEnd - start + buffer.length;
+   }
+
+   int freeBytes() {
+      return buffer.length - bufferedBytes();
+   }
+
+   @Override
+   public void tick(long tick) {
+      currentTick = tick;
+      handleNewData();
+   }
+
+   @Override
+   public int available() throws IOException {
+      if (delay == 0)
+         handleNewData();
+      return bufferedBytes(currentVirtualEnd);
+   }
+
+   void handleNewData() {
+      try {
+         int previousEnd = end;
+
+         if (in.available() > 0 && freeBytes() > 0) {
+            int toRead = Math.min(freeBytes(), in.available());
+            int rd;
+            while (toRead > 0) {
+               rd = in.read(buffer, end, Math.min(toRead, buffer.length - end));
+               if (rd == -1)
+                  break; // TODO: handle stream closing
+               toRead -= rd;
+               end += rd;
+               if (end >= buffer.length)
+                  end -= buffer.length;
+            }
+         }
+
+         if (end != previousEnd && delay > 0) {
+            tick_virtualEnd.put(currentTick + delay - 1, end);
+            tickMarks.add(currentTick + delay - 1);
+         }
+      } catch (IOException e) {
+         // TODO handle exceptions
+         throw new RuntimeException(e);
+      }
+
+      while (!tickMarks.isEmpty() && tickMarks.peek() <= currentTick)
+         currentVirtualEnd = tick_virtualEnd.remove(tickMarks.poll());
+
+      if (tickMarks.isEmpty())
+         currentVirtualEnd = end;
+   }
+
+}
---
+++ spiceJ/core/src/test/java/at/borkowski/spicej/streams/DelayedInputStreamTest.java
@@ -0,0 +1,333 @@
+package at.borkowski.spicej.streams;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.util.Random;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import at.borkowski.spicej.impl.SimulationTickSource;
+
+public class DelayedInputStreamTest {
+
+   DelayedInputStream sut0;
+   DelayedInputStream sut1;
+   DelayedInputStream sutn;
+   SimulationTickSource t;
+
+   PipedOutputStream feed0, feed1, feedn;
+
+   public static final int BUFFER = 50;
+   public static final int DELAY = 20;
+
+   @Before
+   public void setUp() throws IOException {
+      t = new SimulationTickSource();
+
+      PipedInputStream pis0, pis1, pisn;
+
+      feed0 = new PipedOutputStream(pis0 = new PipedInputStream());
+      sut0 = new DelayedInputStream(t, pis0, 0, BUFFER);
+      feed1 = new PipedOutputStream(pis1 = new PipedInputStream());
+      sut1 = new DelayedInputStream(t, pis1, 1, BUFFER);
+      feedn = new PipedOutputStream(pisn = new PipedInputStream());
+      sutn = new DelayedInputStream(t, pisn, DELAY, BUFFER);
+
+      t.advance();
+      t.advance();
+      t.advance();
+      t.advance();
+      t.advance();
+   }
+
+   @Test
+   public void testOneByteDelay0() throws IOException {
+      DelayedInputStream sut = sut0;
+      PipedOutputStream feed = feed0;
+
+      assertEquals(0, sut.available());
+      assertEquals(0, sut.bufferedBytes());
+      assertEquals(BUFFER, sut.freeBytes());
+
+      feed.write(13);
+
+      assertEquals(1, sut.available());
+      assertEquals(1, sut.bufferedBytes());
+      assertEquals(BUFFER - 1, sut.freeBytes());
+
+      assertEquals(13, sut.read());
+
+      assertEquals(0, sut.available());
+      assertEquals(0, sut.bufferedBytes());
+      assertEquals(BUFFER, sut.freeBytes());
+   }
+
+   @Test
+   public void testOneByteDelay1() throws IOException {
+      DelayedInputStream sut = sut1;
+      PipedOutputStream feed = feed1;
+
+      assertEquals(0, sut.available());
+      assertEquals(0, sut.bufferedBytes());
+      assertEquals(BUFFER, sut.freeBytes());
+
+      feed.write(13);
+
+      assertEquals(0, sut.available());
+      assertEquals(0, sut.bufferedBytes());
+      assertEquals(BUFFER, sut.freeBytes());
+
+      t.advance();
+
+      assertEquals(1, sut.available());
+      assertEquals(1, sut.bufferedBytes());
+      assertEquals(BUFFER - 1, sut.freeBytes());
+
+      assertEquals(13, sut.read());
+      assertEquals(0, sut.available());
+      assertEquals(0, sut.bufferedBytes());
+      assertEquals(BUFFER, sut.freeBytes());
+   }
+
+   @Test
+   public void testOneByte() throws IOException {
+      DelayedInputStream sut = sutn;
+      PipedOutputStream feed = feedn;
+
+      assertEquals(0, sut.available());
+      assertEquals(0, sut.bufferedBytes());
+      assertEquals(BUFFER, sut.freeBytes());
+
+      feed.write(12);
+
+      assertEquals(0, sut.available());
+      assertEquals(0, sut.bufferedBytes());
+      assertEquals(BUFFER, sut.freeBytes());
+
+      t.advance();
+
+      assertEquals(0, sut.available());
+      assertEquals(1, sut.bufferedBytes());
+      assertEquals(BUFFER - 1, sut.freeBytes());
+
+      for (int i = 0; i < DELAY - 2; i++)
+         t.advance();
+
+      assertEquals(0, sut.available());
+      t.advance();
+      assertEquals(1, sut.available());
+
+      assertEquals(12, sut.read());
+      assertEquals(0, sut.available());
+      assertEquals(0, sut.bufferedBytes());
+      assertEquals(BUFFER, sut.freeBytes());
+   }
+
+   @Test
+   public void testManyBytes() throws IOException {
+      byte[] blk = { 3, 1, 4, 1, 2, 7, 1, 3, 1, 3, 3, 7 };
+
+      DelayedInputStream sut = sutn;
+      PipedOutputStream feed = feedn;
+
+      assertEquals(0, sut.available());
+      assertEquals(0, sut.bufferedBytes());
+      assertEquals(BUFFER, sut.freeBytes());
+
+      feed.write(blk);
+
+      assertEquals(0, sut.available());
+      assertEquals(0, sut.bufferedBytes());
+      assertEquals(BUFFER, sut.freeBytes());
+
+      t.advance();
+
+      assertEquals(0, sut.available());
+      assertEquals(blk.length, sut.bufferedBytes());
+      assertEquals(BUFFER - blk.length, sut.freeBytes());
+
+      for (int i = 0; i < DELAY - 2; i++)
+         t.advance();
+
+      assertEquals(0, sut.available());
+      t.advance();
+      assertEquals(blk.length, sut.available());
+
+      byte[] rd = new byte[blk.length];
+      rd[0] = (byte) sut.read();
+      assertEquals(blk.length - 1, sut.available());
+      assertEquals(blk.length - 1, sut.bufferedBytes());
+      assertEquals(BUFFER - blk.length + 1, sut.freeBytes());
+
+      rd[1] = (byte) sut.read();
+      assertEquals(blk.length - 2, sut.available());
+      assertEquals(blk.length - 2, sut.bufferedBytes());
+      assertEquals(BUFFER - blk.length + 2, sut.freeBytes());
+
+      int done = 2;
+      while (done < rd.length)
+         done += sut.read(rd, done, rd.length - done);
+
+      assertArrayEquals(blk, rd);
+
+      assertEquals(0, sut.available());
+      assertEquals(0, sut.bufferedBytes());
+      assertEquals(BUFFER, sut.freeBytes());
+   }
+
+   @Test
+   public void testManyBytesInterleaved() throws IOException {
+      byte[] blkA = { 3, 1, 4, 1, 2, 7, 1, 3, 1, 3, 3, 7 };
+      byte[] blkB = { 10, 11, 12, 13, 0, (byte) 0xCA, (byte) 0xFE, 0x77 };
+
+      int interval = 7;
+
+      DelayedInputStream sut = sutn;
+      PipedOutputStream feed = feedn;
+
+      assertEquals(0, sut.available());
+      assertEquals(0, sut.bufferedBytes());
+      assertEquals(BUFFER, sut.freeBytes());
+
+      feed.write(blkA);
+
+      assertEquals(0, sut.available());
+      assertEquals(0, sut.bufferedBytes());
+      assertEquals(BUFFER, sut.freeBytes());
+
+      t.advance();
+
+      assertEquals(0, sut.available());
+      assertEquals(blkA.length, sut.bufferedBytes());
+      assertEquals(BUFFER - blkA.length, sut.freeBytes());
+
+      for (int i = 0; i < interval - 1; i++)
+         t.advance();
+
+      assertEquals(0, sut.available());
+      assertEquals(blkA.length, sut.bufferedBytes());
+      assertEquals(BUFFER - blkA.length, sut.freeBytes());
+
+      feed.write(blkB);
+
+      assertEquals(0, sut.available());
+      assertEquals(blkA.length, sut.bufferedBytes());
+      assertEquals(BUFFER - blkA.length, sut.freeBytes());
+
+      t.advance();
+
+      assertEquals(0, sut.available());
+      assertEquals(blkA.length + blkB.length, sut.bufferedBytes());
+      assertEquals(BUFFER - blkA.length - blkB.length, sut.freeBytes());
+
+      for (int i = 0; i < DELAY - interval - 2; i++)
+         t.advance();
+
+      assertEquals(0, sut.available());
+      t.advance();
+      assertEquals(blkA.length, sut.available());
+
+      byte[] rdA = new byte[blkA.length];
+      rdA[0] = (byte) sut.read();
+      assertEquals(blkA.length - 1, sut.available());
+      assertEquals(blkA.length + blkB.length - 1, sut.bufferedBytes());
+      assertEquals(BUFFER - blkA.length - blkB.length + 1, sut.freeBytes());
+
+      t.advance();
+      assertEquals(blkA.length - 1, sut.available());
+      assertEquals(blkA.length + blkB.length - 1, sut.bufferedBytes());
+      assertEquals(BUFFER - blkA.length - blkB.length + 1, sut.freeBytes());
+
+      int done = 1;
+      while (done < rdA.length)
+         done += sut.read(rdA, done, rdA.length - done);
+
+      assertArrayEquals(blkA, rdA);
+
+      assertEquals(0, sut.available());
+      assertEquals(blkB.length, sut.bufferedBytes());
+      assertEquals(BUFFER - blkB.length, sut.freeBytes());
+
+      for (int i = 0; i < interval - 2; i++)
+         t.advance();
+
+      assertEquals(0, sut.available());
+      t.advance();
+      assertEquals(blkB.length, sut.available());
+
+      byte[] rdB = new byte[blkB.length];
+      rdB[0] = (byte) sut.read();
+      assertEquals(blkB.length - 1, sut.available());
+      assertEquals(blkB.length - 1, sut.bufferedBytes());
+      assertEquals(BUFFER - blkB.length + 1, sut.freeBytes());
+
+      t.advance();
+      assertEquals(blkB.length - 1, sut.available());
+      assertEquals(blkB.length - 1, sut.bufferedBytes());
+      assertEquals(BUFFER - blkB.length + 1, sut.freeBytes());
+
+      done = 1;
+      while (done < rdB.length)
+         done += sut.read(rdB, done, rdB.length - done);
+
+      assertArrayEquals(blkB, rdB);
+   }
+
+   @Test
+   public void testWrapAround() throws IOException {
+      byte[] blkA = new byte[BUFFER - 5];
+      Random random = new Random();
+
+      DelayedInputStream sut = sut1;
+      PipedOutputStream feed = feed1;
+
+      for (int i = 0; i < 4; i++) {
+
+         random.nextBytes(blkA);
+
+         assertEquals(0, sut.available());
+         assertEquals(0, sut.bufferedBytes());
+         assertEquals(BUFFER, sut.freeBytes());
+
+         feed.write(blkA);
+
+         assertEquals(0, sut.available());
+         assertEquals(0, sut.bufferedBytes());
+         assertEquals(BUFFER, sut.freeBytes());
+
+         t.advance();
+
+         assertEquals(blkA.length, sut.available());
+         assertEquals(blkA.length, sut.bufferedBytes());
+         assertEquals(BUFFER - blkA.length, sut.freeBytes());
+
+         byte[] rdA = new byte[blkA.length];
+         rdA[0] = (byte) sut.read();
+         assertEquals(blkA.length - 1, sut.available());
+         assertEquals(blkA.length - 1, sut.bufferedBytes());
+         assertEquals(BUFFER - blkA.length + 1, sut.freeBytes());
+
+         t.advance();
+         assertEquals(blkA.length - 1, sut.available());
+         assertEquals(blkA.length - 1, sut.bufferedBytes());
+         assertEquals(BUFFER - blkA.length + 1, sut.freeBytes());
+
+         int done = 1;
+
+         while (done < rdA.length)
+            done += sut.read(rdA, done, rdA.length - done);
+
+         assertArrayEquals(blkA, rdA);
+
+         assertEquals(0, sut.available());
+         assertEquals(0, sut.bufferedBytes());
+         assertEquals(BUFFER, sut.freeBytes());
+      }
+
+   }
+}

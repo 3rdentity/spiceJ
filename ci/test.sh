05d968c84a4bd440d37a4b6ed6295f808ccacf3f
Michael Borkowski <michael@borkowski.at>
2015-02-20 01:39:08 +0100

    (ci) calling ci profile

--- ci/test.sh
+++ ci/test.sh
@@ -4,5 +4,5 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd "$DIR/../spiceJ"
 
-# -P release to activate release-like things like javadoc generation (and warnings)
-mvn -P release clean verify
+# -P ci to activate release-like things like javadoc generation (and warnings)
+mvn -P ci clean verify
